20d4355514294b7dc470e1d62ebfd4508063b4c6
SomaElefanti <elefantisoma@ufl.edu>
2024-12-02 21:41:38 -0500

    Made a template for calculating the distances and then loading them into a vector of pairs

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <utility>
 
 using namespace std;
 
@@ -32,8 +33,22 @@ unordered_map<string, vector<string>> create_map(string filename) {
     return map;
 }
 
+float calculate_distance(float lat_click, float long_click, string lat_record, string long_record) {
+    //dummy function
+}
+
 int main() {
     unordered_map<string, vector<string>> squamates = create_map("squamata_test.csv");
+
+    //get the coordinates from the map image
+    float latitude = 0.0; //dummy
+    float longitude = 0.0; //dummy
+    vector<pair<string, float>> distances;
+
+    //calculate the distances and create
+    for (const auto& pair : squamates) {
+        distances.emplace_back(pair.first, calculate_distance(latitude, longitude, pair.second[2], pair.second[3]));
+    }
 
 
     return 0;
